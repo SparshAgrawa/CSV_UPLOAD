1f4e64ae20287f4d4f1a77010b9df5cf11d859f9
Sparsh Agrawal <sparsh7037@gmail.com>
2023-07-27 02:49:53 +0530

    initial commit

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/CSV_UPLOADS_DB");
+
+const db = mongoose.connection;
+
+db.once("open", function () {
+  console.log("database connected to the server successfully!");
+});
+
+module.exports = db;
